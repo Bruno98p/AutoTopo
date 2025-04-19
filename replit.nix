3f40579b86543202e1d154f2aee2ab8c568ce295
Bruno98p <96922759+Bruno98p@users.noreply.github.com>
2025-04-19 00:55:45 +0000

    Pre-Recovery checkpoint

---
+++ replit.nix
@@ -0,0 +1,11 @@
+
+ { pkgs }: {
+   deps = [
+     pkgs.python311
+     pkgs.python311Packages.pip
+     pkgs.python311Packages.setuptools
+     pkgs.python311Packages.wheel
+     pkgs.python311Packages.pydantic
+     pkgs.python311Packages.fastapi
+     pkgs.python311Packages.uvicorn
+   ];
